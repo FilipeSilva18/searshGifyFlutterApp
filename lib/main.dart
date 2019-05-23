0679084866754306bce3619f90524ac2fcd5bb3b
Filipe Silva <filipe.silva@zup.com.br>
2019-05-22 22:37:54 -0300

    find GIFs

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_giphy_app/ui/home_page.dart';
+
+void main() => runApp(MaterialApp(
+      home: HomePage(),
+      theme: ThemeData(hintColor: Colors.white),
+    ));
